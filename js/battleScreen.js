d48d86f877bdeba44248fb1d0b3d92a1e1909bee
nanter1986 <nanter1986@gmail.com>
2018-02-01 18:32:53 +0200

    beautify

--- js/battleScreen.js
+++ js/battleScreen.js
@@ -1,177 +1,180 @@
 var player1;
 var monster1;
-var playersTurn=true;
-var gameRunning=true;
+var playersTurn = true;
+var gameRunning = true;
 
 
-function battleLoader(){
-    makePlayer();
-    makeMonster();
-    displayPlayerStats();
-    displayMonsterStats();
-    writeCommentary("You encountered a <b>"+monster1.name+"</b>.Get ready for battle!<br>"+
-    "-Attack with the sword.<br>"+
-    "-Block any attack but lose shield points with the shield<br>"+
+function battleLoader() {
+  makePlayer();
+  makeMonster();
+  displayPlayerStats();
+  displayMonsterStats();
+  writeCommentary("You encountered a <b>" + monster1.name + "</b>.Get ready for battle!<br>" +
+    "-Attack with the sword.<br>" +
+    "-Block any attack but lose shield points with the shield<br>" +
     "-Heal with potions.");
 }
 
 function swordClick() {
-    winCondition();
-    if(playersTurn==true && gameRunning==true){
-        playersTurn=false;
-        var actualDamage=0;
-        var damage=player1.sword/monster1.shield;
-        if(damage<1){
-            actualDamage=1;
-        }else{
-            actualDamage=Math.round(damage);
-        }
-        monster1.health=monster1.health-actualDamage;
-        displayMonsterStats();
-        writeCommentary("You dealt <b>"+actualDamage+"</b> points of damage");
+  winCondition();
+  if (playersTurn == true && gameRunning == true) {
+    playersTurn = false;
+    var actualDamage = 0;
+    var damage = player1.sword / monster1.shield;
+    if (damage < 1) {
+      actualDamage = 1;
+    } else {
+      actualDamage = Math.round(damage);
     }
-    setTimeout(beastTurn,2000);
+    monster1.health = monster1.health - actualDamage;
+    displayMonsterStats();
+    writeCommentary("You dealt <b>" + actualDamage + "</b> points of damage");
+  }
+  setTimeout(beastTurn, 2000);
 }
 
 function beastTurn() {
-    winCondition();
-    if(playersTurn==false && gameRunning==true){
-        var actualDamage=0;
-        var damage=monster1.sword/player1.shield;
-        if(damage<1){
-            actualDamage=1;
-        }else{
-            actualDamage=Math.round(damage);
-        }
-        player1.health=player1.health-actualDamage;
-        displayPlayerStats();
-        writeCommentary("You received <b>"+actualDamage+"</b> points of damage!<br><b>OUCH!</b>");
+  winCondition();
+  if (playersTurn == false && gameRunning == true) {
+    var actualDamage = 0;
+    var damage = monster1.sword / player1.shield;
+    if (damage < 1) {
+      actualDamage = 1;
+    } else {
+      actualDamage = Math.round(damage);
     }
-    playersTurn=true;
-    setTimeout(winCondition,1000);
+    player1.health = player1.health - actualDamage;
+    displayPlayerStats();
+    writeCommentary("You received <b>" + actualDamage + "</b> points of damage!<br><b>OUCH!</b>");
+  }
+  playersTurn = true;
+  setTimeout(winCondition, 1000);
 
 }
 
 function shieldClick() {
-    winCondition();
+  winCondition();
 }
 
 function potionClick() {
-    winCondition();
-    if(player1.potion>0){
-        playersTurn=false;
-        player1.potion=player1.potion-1;
-        player1.health+=10;
-        displayPlayerStats();
-        writeCommentary("You used a potion and gained 10 health!");
-        setTimeout(beastTurn,2000);
-    }
+  winCondition();
+  if (player1.potion > 0) {
+    playersTurn = false;
+    player1.potion = player1.potion - 1;
+    player1.health += 10;
+    displayPlayerStats();
+    writeCommentary("You used a potion and gained 10 health!");
+    setTimeout(beastTurn, 2000);
+  }
 }
 
-function winCondition(){
-    if(player1.health<=0){
-        gameRunning=false;
-        var curLoc={lat: 35.50915947260658, lng: 24.022094434738168};
-        localStorage.setItem("currentLocation",JSON.stringify(curLoc));
-        writeCommentary("You lost!");
-        setTimeout(function(){
-            window.location.href="index.html";
-        },2000);
+function winCondition() {
+  if (player1.health <= 0) {
+    gameRunning = false;
+    var curLoc = {
+      lat: 35.50915947260658,
+      lng: 24.022094434738168
+    };
+    localStorage.setItem("currentLocation", JSON.stringify(curLoc));
+    writeCommentary("You lost!");
+    setTimeout(function() {
+      window.location.href = "index.html";
+    }, 2000);
 
-    }else if(monster1.health<=0){
-        gameRunning=false;
-        writeCommentary("You won!");
-        setTimeout(saveData,1000);
-    }
+  } else if (monster1.health <= 0) {
+    gameRunning = false;
+    writeCommentary("You won!");
+    setTimeout(saveData, 1000);
+  }
 }
 
-function saveData(){
-    var beastEncounters=[];
-    if(JSON.parse(localStorage.getItem("beastsEncountered"))==null){
-        beastEncounters.push(monster1.id);
-        localStorage.setItem("beastsEncountered",JSON.stringify(beastEncounters));
-    }else{
-        beastEncounters=JSON.parse(localStorage.getItem("beastsEncountered"));
-        console.log(beastEncounters);
-        beastEncounters.push(monster1.id);
-        localStorage.setItem("beastsEncountered",JSON.stringify(beastEncounters));
-        console.log(beastEncounters);
-    }
-    localStorage.setItem("potionAvailable",player1.potion.size);
-    window.location.href="index.html";
+function saveData() {
+  var beastEncounters = [];
+  if (JSON.parse(localStorage.getItem("beastsEncountered")) == null) {
+    beastEncounters.push(monster1.id);
+    localStorage.setItem("beastsEncountered", JSON.stringify(beastEncounters));
+  } else {
+    beastEncounters = JSON.parse(localStorage.getItem("beastsEncountered"));
+    console.log(beastEncounters);
+    beastEncounters.push(monster1.id);
+    localStorage.setItem("beastsEncountered", JSON.stringify(beastEncounters));
+    console.log(beastEncounters);
+  }
+  localStorage.setItem("potionAvailable", player1.potion.size);
+  window.location.href = "index.html";
 }
 
-function writeCommentary(text){
-    document.getElementById("commentary").innerHTML=text;
+function writeCommentary(text) {
+  document.getElementById("commentary").innerHTML = text;
 }
 
 function displayPlayerStats() {
-    document.getElementById("playerStats").innerHTML=
-    "SWORD: "+player1.sword+
-    "<br>SHIELD: "+player1.shield+
-    "<br>POTION: "+player1.potion+
-    "<br>HEALTH: "+player1.health;
+  document.getElementById("playerStats").innerHTML =
+    "SWORD: " + player1.sword +
+    "<br>SHIELD: " + player1.shield +
+    "<br>POTION: " + player1.potion +
+    "<br>HEALTH: " + player1.health;
 }
 
 function displayMonsterStats() {
-    document.getElementById("monsterStats").innerHTML=
-    "<strong>"+monster1.name+"</strong>"+
-    "<br>SWORD: "+monster1.sword+
-    "<br>SHIELD: "+monster1.shield+
-    "<br>HEALTH: "+monster1.health;
+  document.getElementById("monsterStats").innerHTML =
+    "<strong>" + monster1.name + "</strong>" +
+    "<br>SWORD: " + monster1.sword +
+    "<br>SHIELD: " + monster1.shield +
+    "<br>HEALTH: " + monster1.health;
 }
 
-function makePlayer(){
-    var sw=JSON.parse(localStorage.getItem("swordFound"));
-    if(sw==undefined){
-        sw=[];
-    }
-    var sh=JSON.parse(localStorage.getItem("shieldFound"));
-    if(sh==undefined){
-        sh=[];
-    }
-    var pot=localStorage.getItem("potionAvailable");
-    if(pot==undefined){
-        pot=[];
-    }
-    player1=new Player(sw.length,sh.length,pot.length);
+function makePlayer() {
+  var sw = JSON.parse(localStorage.getItem("swordFound"));
+  if (sw == undefined) {
+    sw = [];
+  }
+  var sh = JSON.parse(localStorage.getItem("shieldFound"));
+  if (sh == undefined) {
+    sh = [];
+  }
+  var pot = localStorage.getItem("potionAvailable");
+  if (pot == undefined) {
+    pot = [];
+  }
+  player1 = new Player(sw.length, sh.length, pot.length);
 }
 
-function makeMonster(){
-    var nameAndId=JSON.parse(localStorage.getItem("currentMonster"));
-    var monsterNameLoad=nameAndId[0];
-    var id=0;
-    var sword=1;
-    var shield=1;
-    var health=1;
-    var image="img/black.jpg";
-    switch (monsterNameLoad) {
-        case "chimera":
-            sword=10;
-            shield=5;
-            health=10;
-            image="img/threeHeaded.jpg";
-            id=nameAndId[1];
-            break;
-        default:
+function makeMonster() {
+  var nameAndId = JSON.parse(localStorage.getItem("currentMonster"));
+  var monsterNameLoad = nameAndId[0];
+  var id = 0;
+  var sword = 1;
+  var shield = 1;
+  var health = 1;
+  var image = "img/black.jpg";
+  switch (monsterNameLoad) {
+    case "chimera":
+      sword = 10;
+      shield = 5;
+      health = 10;
+      image = "img/threeHeaded.jpg";
+      id = nameAndId[1];
+      break;
+    default:
 
-    }
-    monster1=new Monster(monsterNameLoad,sword,shield,health,image,id);
-    document.getElementById("monsterImage").src=monster1.image;
+  }
+  monster1 = new Monster(monsterNameLoad, sword, shield, health, image, id);
+  document.getElementById("monsterImage").src = monster1.image;
 }
 
-function Player(sword,shield,potion){
-    this.sword=sword;
-    this.shield=shield;
-    this.potion=potion;
-    this.health=10;
+function Player(sword, shield, potion) {
+  this.sword = sword;
+  this.shield = shield;
+  this.potion = potion;
+  this.health = 10;
 }
 
-function Monster(name,sword,shield,health,image,id){
-    this.name=name;
-    this.sword=sword;
-    this.shield=shield;
-    this.health=health;
-    this.image=image;
-    this.id=id;
+function Monster(name, sword, shield, health, image, id) {
+  this.name = name;
+  this.sword = sword;
+  this.shield = shield;
+  this.health = health;
+  this.image = image;
+  this.id = id;
 }
--- js/index.js
+++ js/index.js
@@ -1,169 +1,172 @@
-
 var panorama;
-var sword=[];
-var shield=[];
-var potion=[];
-var potionAvailable=0;
-var beastEncounters=[];
-var audio=new Audio("sound/tad.m4a");
+var sword = [];
+var shield = [];
+var potion = [];
+var potionAvailable = 0;
+var beastEncounters = [];
+var audio = new Audio("sound/tad.m4a");
+
 function streetViewMy() {
-    var curLoc;
-    if (JSON.parse(localStorage.getItem("currentLocation")) != null) {
-        curLoc = JSON.parse(localStorage.getItem("currentLocation"));
+  var curLoc;
+  if (JSON.parse(localStorage.getItem("currentLocation")) != null) {
+    curLoc = JSON.parse(localStorage.getItem("currentLocation"));
+  } else {
+    curLoc = {
+      lat: 35.50915947260658,
+      lng: 24.022094434738168
+    };
+    localStorage.setItem("currentLocation", JSON.stringify(curLoc));
+  }
+  panorama = new google.maps.StreetViewPanorama(
+    document.getElementById('pano'), {
+      position: curLoc,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+      fullscreenControl: false,
+      zoomControl: false,
+      scrollwheel: false,
+      zoom: 0,
+      panControl: false,
+      addressControl: false
+    });
+  panorama.position_changed = function() {
+    recalculate();
+    console.log("position changed");
+  }
+  if (localStorage.getItem("swordFound") != null) {
+    sword = JSON.parse(localStorage.getItem("swordFound"));
+    console.log("sword array found" + sword);
+    if (localStorage.getItem("shieldFound") != null) {
+      shield = JSON.parse(localStorage.getItem("shieldFound"));
+      console.log("shield array found " + shield);
+    }
+    if (localStorage.getItem("potionFound") != null) {
+      potion = JSON.parse(localStorage.getItem("potionFound"));
+      console.log("potion array found " + potion);
+    }
+    if (localStorage.getItem("potionAvailable") != null) {
+      potionAvailable = localStorage.getItem("potionAvailable");
+      console.log("potion av found " + potionAvailable);
+    }
+    if (localStorage.getItem("beastsEncountered") != null) {
+      beastEncounters = JSON.parse(localStorage.getItem("beastsEncountered"));
+      console.log("beast Encounters array found " + beastEncounters);
+    }
+    console.log("str view ready!");
+  }
+
+  function recalculate() {
+
+    var existsAlready = false;
+    console.log("here: " + panorama.position + "___heading:" + panorama.getPov().heading + "___pitch:" + panorama.getPov().pitch);
+    var toRed = createRandomness();
+
+    document.getElementById("textInfo").innerHTML = panorama.location.description + "<br>" + "here: " + panorama.position + "<br>heading:" + panorama.getPov().heading + "<br>pitch:" + panorama.getPov().pitch +
+      "<br>sword found: " + sword.length +
+      "<br>shield found: " + shield.length +
+      "<br>potion found: " + potionAvailable;
+
+    if (toRed[0] > 80) {
+      proccessEncounter(beastEncounters, toRed);
+
+    } else if (toRed[0] > 30) {
+      document.getElementById("textInfo").style.background = "green";
+      document.getElementById("imgInfo").src = "img/black.jpg"
+      console.log("nothing here");
+    } else if (toRed[0] > 20) {
+      selectFoundItem(sword, toRed, "swordFound", "sword.jpg");
+
+    } else if (toRed[0] > 10) {
+      selectFoundItem(shield, toRed, "shieldFound", "shield.png");
+
+    } else if (toRed[0] > 0) {
+      selectFoundItem(potion, toRed, "potionFound", "potion.jpg");
+
+    }
+    localStorage.setItem("currentLocation", JSON.stringify(panorama.position));
+
+    //streetViewMy();
+  }
+
+  function proccessEncounter(beastEncounters, toRed) {
+    var existsAlready = false;
+    var nameAndId = [];
+    if (beastEncounters.length != 0) {
+      for (var i = 0; i < beastEncounters.length; i++) {
+        if (beastEncounters[i] == toRed[1]) {
+          document.getElementById("textInfo").style.background = "green";
+          document.getElementById("imgInfo").src = "img/black.jpg"
+          existsAlready = true;
+          console.log("already found that beast");
+
+        }
+      }
+    }
+    if (existsAlready == false) {
+      nameAndId.push("chimera");
+      nameAndId.push(toRed[1]);
+      localStorage.setItem("currentMonster", JSON.stringify(nameAndId));
+      console.log("Beast Encounter " + nameAndId[0] + " " + nameAndId[1]);
+      setTimeout(function() {
+        console.log("to battle again bitch :P");
+        console.log(toRed[1]);
+        console.log(beastEncounters);
+        window.location.href = "battleScreen.html";
+      }, 2000);
+    }
+
+
+  }
+
+  function selectFoundItem(array, toRed, localStorageName, photoFileName) {
+    var existsAlready = false;
+    if (array.length != 0) {
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] == toRed[1]) {
+          document.getElementById("textInfo").style.background = "green";
+          document.getElementById("imgInfo").src = "img/black.jpg"
+          existsAlready = true;
+          console.log("already found");
+        }
+      }
+      if (existsAlready == false) {
+        document.getElementById("textInfo").style.background = "red";
+        document.getElementById("imgInfo").src = "img/" + photoFileName;
+        array.push(toRed[1]);
+        localStorage.setItem(localStorageName, JSON.stringify(array));
+        console.log("found new.stringified and saved" + array);
+        if (photoFileName == "potion.jpg") {
+          potionAvailable++;
+        }
+        audio.play();
+
+      }
     } else {
-        curLoc = {lat: 35.50915947260658, lng: 24.022094434738168};
-        localStorage.setItem("currentLocation", JSON.stringify(curLoc));
+      document.getElementById("textInfo").style.background = "red";
+      document.getElementById("imgInfo").src = "img/" + photoFileName;
+      array.push(toRed[1]);
+      localStorage.setItem(localStorageName, JSON.stringify(array));
+      console.log("found new.stringified and saved" + array);
+      if (photoFileName == "potion.jpg") {
+        potionAvailable++;
+      }
+      audio.play();
     }
-    panorama = new google.maps.StreetViewPanorama(
-        document.getElementById('pano'), {
-            position: curLoc,
-            pov: {
-                heading: 34,
-                pitch: 10,
-            },
-            fullscreenControl: false,
-            zoomControl: false,
-            scrollwheel: false,
-            zoom: 0,
-            panControl: false,
-            addressControl: false
-        });
-    panorama.position_changed = function () {
-        recalculate();
-        console.log("position changed");
-    }
-    if (localStorage.getItem("swordFound") != null) {
-        sword = JSON.parse(localStorage.getItem("swordFound"));
-        console.log("sword array found" + sword);
-        if (localStorage.getItem("shieldFound") != null) {
-            shield = JSON.parse(localStorage.getItem("shieldFound"));
-            console.log("shield array found " + shield);
-        }
-        if (localStorage.getItem("potionFound") != null) {
-            potion = JSON.parse(localStorage.getItem("potionFound"));
-            console.log("potion array found " + potion);
-        }
-        if (localStorage.getItem("potionAvailable") != null) {
-            potionAvailable = localStorage.getItem("potionAvailable");
-            console.log("potion av found " + potionAvailable);
-        }
-        if (localStorage.getItem("beastsEncountered") != null) {
-            beastEncounters = JSON.parse(localStorage.getItem("beastsEncountered"));
-            console.log("beast Encounters array found " + beastEncounters);
-        }
-        console.log("str view ready!");
-    }
-
-    function recalculate() {
-
-        var existsAlready = false;
-        console.log("here: " + panorama.position + "___heading:" + panorama.getPov().heading + "___pitch:" + panorama.getPov().pitch);
-        var toRed = createRandomness();
-
-        document.getElementById("textInfo").innerHTML = panorama.location.description + "<br>" + "here: " + panorama.position + "<br>heading:" + panorama.getPov().heading + "<br>pitch:" + panorama.getPov().pitch +
-            "<br>sword found: " + sword.length +
-            "<br>shield found: " + shield.length +
-            "<br>potion found: " + potionAvailable;
-
-        if (toRed[0] > 80) {
-            proccessEncounter(beastEncounters, toRed);
-
-        } else if (toRed[0] > 30) {
-            document.getElementById("textInfo").style.background = "green";
-            document.getElementById("imgInfo").src = "img/black.jpg"
-            console.log("nothing here");
-        } else if (toRed[0] > 20) {
-            selectFoundItem(sword, toRed, "swordFound", "sword.jpg");
-
-        } else if (toRed[0] > 10) {
-            selectFoundItem(shield, toRed, "shieldFound", "shield.png");
-
-        } else if (toRed[0] > 0) {
-            selectFoundItem(potion, toRed, "potionFound", "potion.jpg");
-
-        }
-        localStorage.setItem("currentLocation", JSON.stringify(panorama.position));
-
-        //streetViewMy();
-    }
-
-    function proccessEncounter(beastEncounters, toRed) {
-        var existsAlready = false;
-        var nameAndId = [];
-        if (beastEncounters.length != 0) {
-            for (var i = 0; i < beastEncounters.length; i++) {
-                if (beastEncounters[i] == toRed[1]) {
-                    document.getElementById("textInfo").style.background = "green";
-                    document.getElementById("imgInfo").src = "img/black.jpg"
-                    existsAlready = true;
-                    console.log("already found that beast");
-
-                }
-            }
-        }
-        if (existsAlready == false) {
-            nameAndId.push("chimera");
-            nameAndId.push(toRed[1]);
-            localStorage.setItem("currentMonster", JSON.stringify(nameAndId));
-            console.log("Beast Encounter " + nameAndId[0] + " " + nameAndId[1]);
-            setTimeout(function () {
-                console.log("to battle again bitch :P");
-                console.log(toRed[1]);
-                console.log(beastEncounters);
-                window.location.href = "battleScreen.html";
-            }, 2000);
-        }
+  }
 
 
-    }
-
-    function selectFoundItem(array, toRed, localStorageName, photoFileName) {
-        var existsAlready = false;
-        if (array.length != 0) {
-            for (var i = 0; i < array.length; i++) {
-                if (array[i] == toRed[1]) {
-                    document.getElementById("textInfo").style.background = "green";
-                    document.getElementById("imgInfo").src = "img/black.jpg"
-                    existsAlready = true;
-                    console.log("already found");
-                }
-            }
-            if (existsAlready == false) {
-                document.getElementById("textInfo").style.background = "red";
-                document.getElementById("imgInfo").src = "img/" + photoFileName;
-                array.push(toRed[1]);
-                localStorage.setItem(localStorageName, JSON.stringify(array));
-                console.log("found new.stringified and saved" + array);
-                if (photoFileName == "potion.jpg") {
-                    potionAvailable++;
-                }
-                audio.play();
-
-            }
-        } else {
-            document.getElementById("textInfo").style.background = "red";
-            document.getElementById("imgInfo").src = "img/" + photoFileName;
-            array.push(toRed[1]);
-            localStorage.setItem(localStorageName, JSON.stringify(array));
-            console.log("found new.stringified and saved" + array);
-            if (photoFileName == "potion.jpg") {
-                potionAvailable++;
-            }
-            audio.play();
-        }
-    }
-
-
-    function createRandomness() {
-        var aLocalArray = [];
-        var num1 = 100000 * (panorama.position.lng() + panorama.position.lat());
-        num1 = Math.floor(num1);
-        console.log(num1);
-        var theString = "" + num1
-        var key1 = Number(theString[theString.length - 2] + theString[theString.length - 1]);
-        console.log(key1);
-        aLocalArray[0] = key1;
-        aLocalArray[1] = num1;
-        return aLocalArray;
-    }
+  function createRandomness() {
+    var aLocalArray = [];
+    var num1 = 100000 * (panorama.position.lng() + panorama.position.lat());
+    num1 = Math.floor(num1);
+    console.log(num1);
+    var theString = "" + num1
+    var key1 = Number(theString[theString.length - 2] + theString[theString.length - 1]);
+    console.log(key1);
+    aLocalArray[0] = key1;
+    aLocalArray[1] = num1;
+    return aLocalArray;
+  }
 }
